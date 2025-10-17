1b670009b6f53b720f4af7bd286fd7130b1bd380
nedigan <101230593+nedigan@users.noreply.github.com>
2025-10-17 12:14:26 +1100

    Password All Caps

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ TEMPLATE = """
   </head>
   <body>
     <div class="card">
-      <h1>One-Click Password Generator</h1>
+      <h1>One-Click PASSWORD Generator</h1>
       {% if password %}
         <div class="pwd" id="pwd">{{ password }}</div>
         <button onclick="copy()">Copy</button>
