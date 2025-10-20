33f405ca9d07e0f91d37a224b1c5d3d6e7a76b1e
nedigan <101230593+nedigan@users.noreply.github.com>
2025-10-20 12:29:40 +1100

    Test Server Again

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ TEMPLATE = """
   </head>
   <body>
     <div class="card">
-      <h1>One-Click Password GENERATOR</h1>
+      <h1>One-Click PassworD GENERATOR</h1>
       {% if password %}
         <div class="pwd" id="pwd">{{ password }}</div>
         <button onclick="copy()">Copy</button>
