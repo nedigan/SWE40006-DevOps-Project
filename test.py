405b83fa9aa4555a166b79b4fd95d698ca695629
nedigan <101230593+nedigan@users.noreply.github.com>
2025-10-22 13:08:17 +1100

    Use Style.css

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 def test_files_exists():
-    filepath = "app.py"  # change this to whatever file you want to check
+    filepath = "app.py"
     assert os.path.exists(filepath), f"File not found: {filepath}"
     filepath = "styles.css"
     assert os.path.exists(filepath), f"File not found: {filepath}"
@@ -10,8 +10,8 @@ def test_files_exists():
 if __name__ == "__main__":
     try:
         test_files_exists()
-        print("✅ Files exist!")
+        print("Success: Files exist!")
         sys.exit(0)
     except AssertionError as e:
-        print(f"❌ {e}")
+        print(f"Failed: {e}")
         sys.exit(1)
